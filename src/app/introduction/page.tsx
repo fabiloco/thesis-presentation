c12c682cbc8f8a333250675383214937b63852b3
Fabián Sánchez <faalsaru@gmail.com>
2023-11-16 22:53:56 -0500

    introduction

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -4,7 +4,11 @@ export default function Introduction() {
   return (
     <>
       <Navbar />
-      <section className='mx-auto flex flex-col justify-center min-h-screen max-w-4xl'></section>
+      <section className='mx-auto flex flex-col justify-center min-h-screen max-w-4xl'>
+        <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl'>
+          Introducción
+        </h1>
+      </section>
     </>
   );
 }
