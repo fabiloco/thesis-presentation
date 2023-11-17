a423cd92f0110ae774f201d22a5d3bd6b1b0fecd
Fabián Sánchez <faalsaru@gmail.com>
2023-11-16 22:52:43 -0500

    navbar

---
+++ src/app/introduction/page.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from '@/components';
+
+export default function Introduction() {
+  return (
+    <>
+      <Navbar />
+      <section className='mx-auto flex flex-col justify-center min-h-screen max-w-4xl'></section>
+    </>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import Link from 'next/link';
+
+import React, { FC } from 'react';
 
 export const Navbar = () => {
-  return <div>Navbarasdfasdfasdfasdf</div>;
+  return (
+    <nav>
+      <ul className='flex w-full'>
+        <NavbarLink to='/' content='Inicio' />
+        <NavbarLink to='/introduction' content='Introducción' />
+      </ul>
+    </nav>
+  );
+};
+
+type NavbarLink = {
+  to: string;
+  content: string;
+};
+
+const NavbarLink: FC<NavbarLink> = ({ to, content }) => {
+  return (
+    <li className='px-2 py-1'>
+      <Link href={to}>{content}</Link>
+    </li>
+  );
 };
