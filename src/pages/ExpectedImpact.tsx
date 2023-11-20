366afa98781aba34f6eb08090829312de400ced2
Fabián Sánchez <faalsaru@gmail.com>
2023-11-20 08:26:25 -0500

    final

--- src/pages/ExpectedImpact.tsx
+++ src/pages/ExpectedImpact.tsx
@@ -1,38 +1,40 @@
-import { ExpectedImpactItem } from "../components";
-import "../index.css";
+import { ExpectedImpactItem } from '../components';
+import '../index.css';
 
 export const ExpectedImpact = () => {
   const data = [
     {
-      title: "Area Social",
-      icon: "",
+      title: 'Area Social',
+      icon: '',
     },
     {
-      title: "Area Economica",
+      title: 'Area Economica',
     },
     {
-      title: "Area Ambiental",
+      title: 'Area Ambiental',
     },
     {
-      title: "Area Academica y Cientifica",
+      title: 'Area Academica y Cientifica',
     },
     {
-      title: "Area Tecnologica y Cultural",
+      title: 'Area Tecnologica y Cultural',
     },
   ];
   return (
     <>
-      <div className="flex flex-col justify-center items-center my-5">
-        <h2 className="mb-10 text-4xl font-bold underline underline-offset-3 decoration-4 underline-offset-4 decoration-blue-500">
-          Impacto Esperado
-        </h2>
+      <section id='newTechnologies' className='mx-auto max-w-6xl mt-4'>
+        <div className='pt-40 pb-28 flex justify-center items-center flex-col gap-8'>
+          <h2 className='text-4xl font-bold underline underline-offset-3 decoration-4 underline-offset-4 decoration-blue-500'>
+            Nuevas tecnologías
+          </h2>
+        </div>
 
-        <div className="flex flex-row w-full justify-center items-center">
+        <div className='flex flex-row w-full justify-center items-center'>
           {data.map(({ title }) => (
             <ExpectedImpactItem title={title} />
           ))}
         </div>
-      </div>{" "}
+      </section>{' '}
     </>
   );
 };
